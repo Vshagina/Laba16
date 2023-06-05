b9d40747f1432745d8ac02b146d8ac8faf13c875
Victoria <123573080+Vshagina@users.noreply.github.com>
2023-06-05 12:22:07 +0300

    Add files via upload

---
+++ Laba16.java
@@ -0,0 +1,43 @@
+package com.company;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Laba16{
+    interface Operationable {
+        boolean calculate(int x, int y);
+    }
+
+    private static void selectionSort(int [] array, Operationable uslov) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (uslov.calculate(array[j], array[minIndex])) {
+                    minIndex = j;
+                }
+            }
+            int tmp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите размерность массива:");
+        int size = input.nextInt();
+        int[] array = new int[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++){
+            array[i] = input.nextInt();
+        }
+        System.out.println("Начальный массив: ");
+        System.out.println(Arrays.toString(array));
+        System.out.println("По возрастанию: ");
+        selectionSort(array, ((x, y) -> x < y));
+        System.out.println(Arrays.toString(array));
+        System.out.println("По убыванию: ");
+        selectionSort(array, ((x, y) -> x > y));
+        System.out.println(Arrays.toString(array));
+
+    }
+}
